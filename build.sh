e501c8db2e880bcd1720341b63984af9fb005ec2
dekken <philip.deegan@gmail.com>
2023-11-05 14:44:58 +0100

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 
 set -ex
+BASE_URL="docker.io/"
 FROM_IMAGE=${1:-"gcc:13.2.0"}
-IMAGE="ghcr.io/pharchive/phare_debian/${FROM_IMAGE}"
+IMAGE="ghcr.io/pharchive/phare_debian_dep/${FROM_IMAGE}"
+FROM_IMAGE="${BASE_URL}${FROM_IMAGE}"
 docker build --build-arg FROM_IMAGE=$FROM_IMAGE -t ${IMAGE} .
--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 set -ex
 
 apt-get update
-apt-get install -y git make cmake libopenmpi-dev libhdf5-openmpi-dev g++ ninja-build python3-dev python3-pip gfortran-
+apt-get install -y git make cmake libopenmpi-dev libhdf5-openmpi-dev g++ ninja-build \
+                   ccache python3-dev python3-pip gfortran-
 
 git clone https://github.com/PHAREHUB/PHARE -b master --depth 1 --recursive --shallow-submodules phare
 
